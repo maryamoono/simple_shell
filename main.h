49f49ce31ef96f30b47f06d6057930bd3330be41
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-11-06 16:46:20 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,20 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+
 #define BUFFER_SIZE 1024
+
 void type_prompt(void);
 void clear_in(char *input, char *output);
-#endif
+
+//for with string 
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
+#endif/*MAIN_H*/
