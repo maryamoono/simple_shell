dc791163fab8d7a708cb33b9e3d890f8dba76c35
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-11-07 07:42:43 +0300

    Update main.h

--- main.h
+++ main.h
@@ -12,10 +12,19 @@ void type_prompt(void);
 void clear_in(char *input, char *output);
 char *get_command_loction(char *command);
 
+
+//for with string
 char *_strcat(char *dest, const char *src);
-char *_strcat(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
+
+//for with string 2 
+char *_strdup(const char *s);
+int _strlen(const char *s);
+int cmp_chars(char str[], const char *delim);
+char *_strtok(char str[], const char *delim);
+int _isdigit(const char *s);
 
 #endif/*MAIN_H*/
