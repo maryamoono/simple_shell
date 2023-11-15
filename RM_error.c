8fc56de2a4ea8c7b8fb0e495851d83244c47fd78
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-11-15 18:12:56 +0300

    Update RM_error.c

--- RM_error.c
+++ RM_error.c
@@ -1,54 +1,44 @@
 #include "shell.h"
 
-/**
- * get_error - this function to calls the error 
- * according the builtin, syntax or permission .
+/*
+ * get_error - This function calls the error
+ * according to the builtin, syntax, or permission.
  *
- * @datash: data structure that contains arguments.
- * @error: error value .
+ * @datash: Data structure that contains arguments.
+ * @error: Error value.
  *
- * Return: error.
+ * Return: Error.
  */
 int get_error(data_shell *datash, int error)
-	/**
-	 * get_error - this function to calls the error
-	 * according the builtin, syntax or permission .
-	 *
-	 * @datash: data structure that contains arguments.
-	 * @error: error value .
-	 *
-	 * Return: error.
-	 */
-int get_error(data_shell *datash, int error )
 {
-	char *error_p;
-	/*caces of errores*/
-	switch (error)
-	{
-		case -1:
-			error_p = error_env(datash);
-			break;
-		case 126:
-			error_p = error_path_126(datash);
-			break;
-		case 127:
-			error_p = error_not_found(datash);
-			break;
-		case 2:
-			if (_strcmp("exit", datash->args[0]) == 0)
-				error_p = error_exit_shell(datash);
-			else if (_strcmp("cd", datash->args[0]) == 0)
-				error_p = error_get_cd(datash);
-			break;
-	}
+  char *error_p;
+  /* Cases of errors */
+  switch (error)
+  {
+    case -1:
+      error_p = error_env(datash);
+      break;
+    case 126:
+      error_p = error_path_126(datash);
+      break;
+    case 127:
+      error_p = error_not_found(datash);
+      break;
+    case 2:
+      if (_strcmp("exit", datash->args[0]) == 0)
+        error_p = error_exit_shell(datash);
+      else if (_strcmp("cd", datash->args[0]) == 0)
+        error_p = error_get_cd(datash);
+      break;
+  }
 
-	if (error_p)
-	{
-		write(STDERR_FILENO, error, _strlen(int);
-				free(error_p);
-				}
+  if (error_p)
+  {
+    write(STDERR_FILENO, error_p, _strlen(error_p));
+    free(error_p);
+  }
 
-				datash->status = error;
-				return (error);
-				}
+  datash->status = error;
+  return (error);
+}
 
