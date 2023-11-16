6d7687c1d07d75053427b5982d99fb028c159ffb
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-11-16 10:57:43 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -45,16 +45,15 @@ char *_strtok(char str[], const char *delim);
 int _isdigit(const char *s);
 
 void rev_string(char *s);
-/**
- * struct data - struct that contains all relevant data on runtime.
- *
- * @av: argument vector.
- * @input: command line written by the user.
- * @args: tokens of the command line .
- * @status: last status of the shell .
- * @counter: lines counter .
- * @_environ: environment variable .
- * @pid: process ID of the shell .
+**
+ * struct data - struct that contains all relevant data on runtime
+ * @av: argument vector
+ * @input: command line written by the user
+ * @args: tokens of the command line
+ * @status: last status of the shell
+ * @counter: lines counter
+ * @_environ: environment variable
+ * @pid: process ID of the shell
  */
 typedef struct data
 {
@@ -68,8 +67,48 @@ typedef struct data
 } data_shell;
 
 /**
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: next node
+ * Description: single linked list to store separators
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct line_list_s - single linked list
+ * @line: command line
+ * @next: next node
+ * Description: single linked list to store command lines
+ */
+typedef struct line_list_s
+{
+	char *line;
+	struct line_list_s *next;
+} line_list;
+
+/**
+ * struct r_var_list - single linked list
+ * @len_var: length of the variable
+ * @val: value of the variable
+ * @len_val: length of the value
+ * @next: next node
+ * Description: single linked list to store variables
+ */
+typedef struct r_var_list
+{
+	int len_var;
+	char *val;
+	int len_val;
+	struct r_var_list *next;
+} r_var;
+
+/**
  * struct builtin_s - Builtin struct for command args.
- * @name: The name of the command builtin i.e cd, exit, env
+ * @name: The name of the command builtin i.e cd, env
  * @f: data type pointer function.
  */
 typedef struct builtin_s
