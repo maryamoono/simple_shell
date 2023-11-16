9b24a596d02674af032d4b639eb6e5f388caa824
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-11-16 14:29:42 +0300

    Update com.c

--- com.c
+++ com.c
@@ -70,3 +70,35 @@ int _unsetenv(data_shell *datash)
 	datash->_environ = realloc_environ;
 	return (1);
 }
+/**
+ * error_not_found - this function to generic error message for command not found.
+ * @datash: data relevant (counter, arguments).
+ *
+ * Return: Error message.
+ */
+char *error_not_found(data_shell *datash)
+{
+	int length;
+	char *error;
+	char *ver_str;
+
+	ver_str = RM_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(ver_str);
+	length += _strlen(datash->args[0]) + 16;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(error);
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->args[0]);
+	_strcat(error, ": not found\n");
+	_strcat(error, "\0");
+	free(ver_str);
+	return (error);
+}
